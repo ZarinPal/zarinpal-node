38d70b559aa4f271d526d507a115a0a2f03a0b40
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-12-04 14:17:23 +0330

    test: verify pg test

--- src/tests/PG.test.ts
+++ src/tests/PG.test.ts
@@ -15,3 +15,17 @@ test('create new PG successfully', async () => {
   const res = await zarinpal.pg.newPayment(args);
   expect(res).toContain('https://www.zarinpal.com/pg/StartPay/');
 });
+
+test('verify PG', async () => {
+  const args = {
+    amount: 10000,
+    authority: 'A00000000000000000000000000482585518',
+    merchant_id: '6c64a645-1b28-4956-b32e-7b777864121a',
+  };
+  const zarinpal = new Zarinpal({});
+  const res = await zarinpal.pg.verify(args);
+  expect(res).toMatchObject({
+    code: -51,
+    message: 'Session is not valid, session is not active paid try.',
+  });
+});
